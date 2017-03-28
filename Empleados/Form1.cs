1867981acad6c595b7c79cfd9bb5b13cf644edcc
exxteban <csarlezcano@gmail.com>
2017-03-28 10:14:28 -0400

    primer commit

---
+++ Empleados/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Empleados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            
+            SqlConnection con = new SqlConnection("Data Source=localhost; Initial Catalog=sistemadenomina; Integrated Security=true");
+            
+            try
+            {
+                SqlCommand cmd = new SqlCommand("select nombre, contrasena from usuarios where nombre ='" + txtUsuario.Text + "' and contrasena ='" + txtContrasena.Text + "'", con);
+                con.Open();
+                Console.WriteLine("abierta");
+                cmd.ExecuteNonQuery();
+                DataSet ds = new DataSet();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(ds, "usuarios");
+                DataRow row;
+                row = ds.Tables["usuarios"].Rows[0];
+
+                if ((txtUsuario.Text == row["nombre"].ToString()) || (txtContrasena.Text == row["contrasena"].ToString()))
+                {
+                    FormPrincipal p = new FormPrincipal();
+                    p.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Error al loguearse");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar a la base de datos");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+
+        }
+    }
+}
